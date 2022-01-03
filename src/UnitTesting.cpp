69c15fd22956f0aeb22a9ca7d4b5839f558f08c9
blurdel <blurdel@gmail.com>
2022-01-03 15:41:29 -0500

    updated to use ServiceBus interface

--- src/UnitTesting.cpp
+++ src/UnitTesting.cpp
@@ -30,28 +30,51 @@ struct BrakeCommand {
 	double time2collision_s;
 };
 
-struct ServiceBus {
-	void publish(const BrakeCommand&);
+typedef function<void(const SpeedUpdate&)> SpeedUpdateCallback;
+typedef function<void(const CarDetected&)> CarDetectedCallback;
+
+struct IServiceBus {
+	virtual ~IServiceBus() = default;
+	virtual void publish(const BrakeCommand&) = 0;
+	virtual void subscribe(SpeedUpdateCallback) = 0;
+	virtual void subscribe(CarDetectedCallback) = 0;
 };
 
-template <typename T>
-struct AutoBrake {
-	AutoBrake(const T& publish) :
-		collision_thresh_s{ 5 },
-		speed_mps{},
-		publish{ publish } {}
+struct MockServiceBus : IServiceBus {
+	void publish(const BrakeCommand& cmd) override {
+		commands_published++;
+		last_command = cmd;
+	}
+	void subscribe(SpeedUpdateCallback callback) override {
+		speed_update_callback = callback;
+	}
+	void subscribe(CarDetectedCallback callback) override {
+		car_detected_callback = callback;
+	}
 
-	void observe(const SpeedUpdate& x) {
-		speed_mps = x.velocity_mps;
-	}
-	void observe(const CarDetected& cd) {
-		const auto relative_velocity_mps = speed_mps - cd.velocity_mps;
-		const auto time_to_collision_s = cd.distance_m / relative_velocity_mps;
-//		if (time_to_collision_s > 0 &&
-//				time_to_collision_s <= collision_thresh_s) {
-//			publish(BrakeCommand{ time_to_collision_s });
-//		}
-	}
+	BrakeCommand last_command{};
+	int commands_published{};
+	SpeedUpdateCallback speed_update_callback{};
+	CarDetectedCallback car_detected_callback{};
+};
+
+
+struct AutoBrake {
+	AutoBrake(IServiceBus& bus) :
+		collision_thresh_s{ 5 },
+		speed_mps{} {
+			bus.subscribe([this](const SpeedUpdate& update) {
+				speed_mps = update.velocity_mps;
+			});
+			bus.subscribe([this, &bus](const CarDetected& cd) {
+				auto relative_velocity_mps = speed_mps - cd.velocity_mps;
+				auto time_to_collision_s = cd.distance_m / relative_velocity_mps;
+				if(time_to_collision_s > 0 && time_to_collision_s <= collision_thresh_s) {
+					bus.publish(BrakeCommand{ time_to_collision_s });
+				}
+			});
+		}
+
 
 	void setCollisionThresh(double x) {
 		if (x < 1) {
@@ -69,21 +92,23 @@ struct AutoBrake {
 private:
 	double collision_thresh_s;
 	double speed_mps;
-	const T& publish;
 };
 
 void initialSpeedIsZero() {
-	AutoBrake auto_brake{ [](const BrakeCommand&){} };
+	MockServiceBus bus{};
+	AutoBrake auto_brake{ bus };
 	assert_that(auto_brake.getSpeedMps() == 0L, "speed not equal to 0");
 }
 
 void initialSensitivityIs5() {
-	AutoBrake auto_brake{ [](const BrakeCommand&){} };
+	MockServiceBus bus{};
+	AutoBrake auto_brake{ bus };
 	assert_that(auto_brake.getCollisionThresh() == 5L, "sensitivity is not 5");
 }
 
 void sensitivityGreaterThan1() {
-	AutoBrake auto_brake{ [](const BrakeCommand&){} };
+	MockServiceBus bus{};
+	AutoBrake auto_brake{ bus };
 	try {
 		auto_brake.setCollisionThresh(0.5L);
 	} catch (std::exception& e) {
@@ -93,27 +118,36 @@ void sensitivityGreaterThan1() {
 }
 
 void speedIsSaved() {
-	AutoBrake auto_brake{ [](const BrakeCommand&){} };
-	auto_brake.observe(SpeedUpdate{ 100L });
+	MockServiceBus bus{};
+	AutoBrake auto_brake{ bus };
+
+	bus.speed_update_callback(SpeedUpdate{ 100L });
 	assert_that(100L == auto_brake.getSpeedMps(), "speed not saved to 100");
-	auto_brake.observe(SpeedUpdate{ 50L });
+	bus.speed_update_callback(SpeedUpdate{ 50L });
 	assert_that(50L == auto_brake.getSpeedMps(), "speed not saved to 50");
-	auto_brake.observe(SpeedUpdate{ 0L });
+	bus.speed_update_callback(SpeedUpdate{ 0L });
 	assert_that(0L == auto_brake.getSpeedMps(), "speed not saved to 0");
 }
 
 void alertWhenImminent() {
-	int brake_commands_published{};
-	AutoBrake auto_brake{
-		[&brake_commands_published](const BrakeCommand&) {
-			brake_commands_published++;
-	} };
+	MockServiceBus bus{};
+	AutoBrake auto_brake{ bus };
 	auto_brake.setCollisionThresh(10L);
-	auto_brake.observe(SpeedUpdate{ 100L });
-	auto_brake.observe(CarDetected{ 100L, 0L });
-	assert_that(brake_commands_published == 1, "brake commands published not one");
+
+	bus.speed_update_callback(SpeedUpdate{ 100L });
+	bus.car_detected_callback(CarDetected{ 100L, 0L });
+	assert_that(bus.commands_published == 1, "1 brake command was not published");
+	assert_that(bus.last_command.time2collision_s == 1L, "time to collision not computed correctly");
 }
 
+void no_alert_when_not_imminent() {
+	MockServiceBus bus{};
+	AutoBrake auto_brake{ bus };
+	auto_brake.setCollisionThresh(2L);
+	bus.speed_update_callback(SpeedUpdate{ 100L });
+	bus.car_detected_callback(CarDetected{ 1000L, 50L });
+	assert_that(bus.commands_published == 0, "brake commands were published");
+}
 
 int main() {
 	cout << "" << endl;
@@ -125,18 +159,8 @@ int main() {
 	run_test(initialSensitivityIs5, "initial sensitivity is 5");
 	run_test(sensitivityGreaterThan1, "sensitivity greater than 1");
 	run_test(speedIsSaved, "speed is saved");
+	run_test(no_alert_when_not_imminent, "no alert when not imminent");
 	run_test(alertWhenImminent, "alert when imminent");
-
-//	ServiceBus bus;
-//	AutoBrake auto_brake { [&bus] (const auto& cmd) {
-//		bus.publish(cmd);
-//		}
-//	};
-//
-//	while (true) {
-//		auto_brake.observe( SpeedUpdate{ 10L } );
-//		auto_brake.observe( CarDetected{ 250L, 25l } );
-//	}
 
 	return 0;
 }
